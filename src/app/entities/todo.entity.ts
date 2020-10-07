8a6d50227c4c998a22e58eb3d418e85c8cf40228
bodoganabel <abel.bodogan@zooshgroup.com>
2020-10-07 20:04:36 +0200

    Added culomn: text

--- src/app/entities/todo.entity.ts
+++ src/app/entities/todo.entity.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Todo {
-
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  text: string;
 }
