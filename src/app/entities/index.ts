b41d103814cce99b01e305299cb5e86806f18fe8
bodoganabel <abel.bodogan@zooshgroup.com>
2020-10-07 20:00:27 +0200

    Added todo entity

--- src/app/entities/index.ts
+++ src/app/entities/index.ts
@@ -1,1 +1,2 @@
 export { User } from './user.entity';
+export { Todo } from './todo.entity';
---
+++ src/app/entities/todo.entity.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+}
